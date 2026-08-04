35b1f665a4cc773034e4f920de9b95d9aa5cdc69
Quentin Boileau <qboileau@conduktor.io>
2026-08-04 11:21:44 +0200

    Fix application_instance test

--- internal/testdata/console/application_instance_v1/resource_policy_dependency.tf
+++ internal/testdata/console/application_instance_v1/resource_policy_dependency.tf
@@ -1,4 +1,4 @@
-# Satisfies the `spec.topic_policy_ref` of the complex application instance example.
+# Satisfies the `spec.policy_ref` of the complex application instance example.
 # Kept out of the example HCL so it doesn't show up in the generated docs.
 # Uses ResourcePolicy, the replacement for the TopicPolicy kind removed from Console 1.47.0.
 resource "conduktor_console_resource_policy_v1" "topic_policy_ref" {
