3c9da3fab3ae0b32cafb01fb6f06a43831a425a0
Quentin Boileau <qboileau@conduktor.io>
2026-09-11 10:50:16 +0200

    fix: flag schema_registry.glue.security.iam_anywhere.private_key as sensitive field

--- internal/schema/resource_console_kafka_cluster_v2/console_kafka_cluster_v2_resource_gen.go
+++ internal/schema/resource_console_kafka_cluster_v2/console_kafka_cluster_v2_resource_gen.go
@@ -388,6 +388,7 @@ func ConsoleKafkaClusterV2ResourceSchema(ctx context.Context) schema.Schema {
 													},
 													"private_key": schema.StringAttribute{
 														Required:            true,
+														Sensitive:           true,
 														Description:         "Glue Schema registry AWS private key.",
 														MarkdownDescription: "Glue Schema registry AWS private key.",
 													},
